9065689265c98a54124f50ce4ec3c94bdbbf0ea3
jmatt724 <jmattix@bellarmine.edu>
2021-09-17 18:24:51 -0400

    added Mult and Div

--- src/asm/GenDiv.java
+++ src/asm/GenDiv.java
@@ -14,20 +14,33 @@ public class GenDiv {
         cw.visit(Opcodes.V11, Opcodes.ACC_PUBLIC,"DivideNumbers", null, "java/lang/Object", null);
 		
         {
-        	MethodVisitor mv=cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()v", null, null);
+        	MethodVisitor mv=cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
             mv.visitCode();
             mv.visitVarInsn(Opcodes.ALOAD, 0); // load the first local variable: this
-            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()v",false);
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V",false);
             mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(1,1);
             mv.visitEnd();
         }//end constructor generation
         
         {
         	MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
         	mv.visitCode();
+        	mv.visitLdcInsn((Double)173.43);
+            mv.visitVarInsn(Opcodes.DSTORE,1);
+            mv.visitLdcInsn((Double)45.56);
+            mv.visitVarInsn(Opcodes.DSTORE,3);
+            mv.visitVarInsn(Opcodes.DLOAD,1);
+            mv.visitVarInsn(Opcodes.DLOAD,3);
+            mv.visitInsn(Opcodes.DDIV);
+            mv.visitVarInsn(Opcodes.DSTORE,5);
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitVarInsn(Opcodes.DLOAD, 5);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(D)V", false);
+            
         	
-        	// TODO: main body
-        	
+        	mv.visitInsn(Opcodes.RETURN);
+        	mv.visitMaxs(0, 0);
         	mv.visitEnd();
         }//end main generation
         
--- src/asm/GenMul.java
+++ src/asm/GenMul.java
@@ -14,20 +14,32 @@ public class GenMul {
         cw.visit(Opcodes.V11, Opcodes.ACC_PUBLIC,"MultiplyNumbers", null, "java/lang/Object", null);
 		
         {
-        	MethodVisitor mv=cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()v", null, null);
+        	MethodVisitor mv=cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
             mv.visitCode();
             mv.visitVarInsn(Opcodes.ALOAD, 0); // load the first local variable: this
-            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()v",false);
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V",false);
             mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(1,1);
             mv.visitEnd();
         }//end constructor generation
         
         {
         	MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
         	mv.visitCode();
-        	
-        	// TODO: main body
-        	
+            mv.visitLdcInsn((Double)173.43);
+            mv.visitVarInsn(Opcodes.DSTORE,1);
+            mv.visitLdcInsn((Double)45.56);
+            mv.visitVarInsn(Opcodes.DSTORE,3);
+            mv.visitVarInsn(Opcodes.DLOAD,1);
+            mv.visitVarInsn(Opcodes.DLOAD,3);
+            mv.visitInsn(Opcodes.DMUL);
+            mv.visitVarInsn(Opcodes.DSTORE,5);
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitVarInsn(Opcodes.DLOAD, 5);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(D)V", false);
+            
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(0,0);
         	mv.visitEnd();
         }//end main generation
         
